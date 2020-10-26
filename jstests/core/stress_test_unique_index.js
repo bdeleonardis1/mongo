14ad398469f757aa16d9fe801884121fae0c2485
Brian DeLeonardis <brian.deleonardis@mongodb.com>
2020-10-26 16:02:30 +0000

    Change test to use bulkwrites

--- jstests/core/stress_test_unique_index.js
+++ jstests/core/stress_test_unique_index.js
@@ -1,10 +1,21 @@
+// @tags: [assumes_unsharded_collection]
+
 coll = db.stress_index;
 coll.drop();
 
-const DOC_COUNT = 750000 // ~ 25 MB
+const DOC_COUNT = 2000000 // ~ 25 MB
+const MAX_CHUNK_SIZE = 100000
 
-for (let i = 0; i < DOC_COUNT; i++) {
-    coll.save({ "a": 1 });
+var inserted = 0;
+while (inserted < DOC_COUNT) {
+    var docs = []
+    for (let i = 0; i < MAX_CHUNK_SIZE && inserted + docs.length < DOC_COUNT; i++) {
+        docs.push({"a": 0});
+    }
+    coll.insertMany(docs)
+    inserted += docs.length;
 }
 
-assert.commandFailedWithCode(coll.createIndex({ a: 1 }, { unique: true }), ErrorCodes.DuplicateKey)
+assert.eq(coll.find().count(), DOC_COUNT);
+
+assert.commandFailedWithCode(coll.createIndex({ a: 1 }, { unique: true }), ErrorCodes.DuplicateKey);
