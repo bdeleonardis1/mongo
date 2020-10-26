43a3fd54b0408a72068821d667c6eadd30a6136b
Brian DeLeonardis <brian.deleonardis@mongodb.com>
2020-10-26 02:18:31 +0000

    Lower number of documents

--- jstests/core/stress_test_unique_index.js
+++ jstests/core/stress_test_unique_index.js
@@ -1,7 +1,7 @@
 coll = db.stress_index;
 coll.drop();
 
-const DOC_COUNT = 1500000 // ~ 50 MB
+const DOC_COUNT = 750000 // ~ 25 MB
 
 for (let i = 0; i < DOC_COUNT; i++) {
     coll.save({ "a": 1 });
